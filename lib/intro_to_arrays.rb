295aaeeccc47feb48c0ec2d2b98c95bc10e085c1
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-26 21:28:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,9 +6,9 @@ def array_with_two_elements
   @my_two_array = [1,2]
 end
 
+
 def first_element
-  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-   @taylor_swift[0]
+
 end 
 
 def third_element
