1b41561a545a05c1a9d0f5ed368e20475e120ec1
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-26 21:39:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -27,4 +27,5 @@ def last_element(my_last_element)
 end 
 
 def first_element_with_array_methods(first_country)
-  
+    @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    
