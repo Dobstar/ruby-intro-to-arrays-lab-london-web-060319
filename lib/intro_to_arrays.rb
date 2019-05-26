54335968ed99bf2f3b5f27c5fb1e450555b6a5d0
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-26 21:33:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,10 +14,10 @@ def first_element(@taylor_swift)
 end 
 
 def third_element(@taylor_swift)
-  @taylor_swift = []
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   @taylor_swift[2]
 end
 
-def last_element
+def last_element(@taylor_swift)
   @taylor_swift[3]
 end 
