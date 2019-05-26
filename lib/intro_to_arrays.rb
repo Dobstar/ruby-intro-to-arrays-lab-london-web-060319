b19d6c20be7f1200abe6df4abb81dfcd93ee41ca
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-26 21:46:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -39,5 +39,7 @@ def first_element_with_array_methods(first_country)
   end 
 
 def length_of_array(length)
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  length = @programming_languages#
   
 end
